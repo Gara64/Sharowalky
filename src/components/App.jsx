ad52fd64ed751d13f59f60fe18778a1ed6f27cd1
Gara64 <paul@cozycloud.cc>
2017-05-26 17:19:04 +0200

    Add traces class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
-import styles from '../styles/app'
+// import styles from '../styles/app'
 
 import React from 'react'
 import { translate } from '../lib/I18n'
-import classNames from 'classnames'
+// import classNames from 'classnames'
+import Traces from './traces.jsx'
 
 const App = ({ t }) => (
-  <h1 className={classNames(styles['title'])}>{ t('App.welcome') }</h1>
+    // <h1 className={classNames(styles['title'])}>{ t('App.welcome') }</h1>
+    React.createElement('div', {},
+        React.createElement(Traces, {})
+    )
 )
 
 export default translate()(App)
---
+++ src/components/Traces.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { translate } from '../lib/I18n'
+
+class Traces extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Sharowalky 2000</h1>
+      </div>
+    )
+  }
+}
+
+export default translate()(Traces)
