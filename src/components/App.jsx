a180e5adb1253246acc694e125c9b381eff90e8d
Gara64 <paul@cozycloud.cc>
2017-05-26 19:38:15 +0200

    Add plotly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import { translate } from '../lib/I18n'
 // import classNames from 'classnames'
-import Traces from './traces.jsx'
+import Traces from './Traces.jsx'
 
 const App = ({ t }) => (
     // <h1 className={classNames(styles['title'])}>{ t('App.welcome') }</h1>
--- src/components/Traces.jsx
+++ src/components/Traces.jsx
@@ -1,11 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { translate } from '../lib/I18n'
+import Plotly from 'plotly.js'
 
-class Traces extends Component {
+class Traces extends React.Component {
+  componentDidMount () {
+    Plotly.newPlot('plot', [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16] }]
+    )
+  }
+
   render () {
     return (
       <div>
         <h1>Sharowalky 2000</h1>
+        <div id="plot">
+        </div>
       </div>
     )
   }
