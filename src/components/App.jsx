61261f16b4bdee698ce6694c13fbc9530d639565
Gara64 <paul@cozycloud.cc>
2017-05-28 02:14:29 +0200

    Add map

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,14 @@ import React from 'react'
 import { translate } from '../lib/I18n'
 // import classNames from 'classnames'
 import Traces from './Traces.jsx'
+import Map from './Map.jsx'
 
 const App = ({ t }) => (
     // <h1 className={classNames(styles['title'])}>{ t('App.welcome') }</h1>
     React.createElement('div', {},
-        React.createElement(Traces, {})
+        React.createElement(Traces, {}),
+        React.createElement('div', {id: 'map-container'},
+          React.createElement(Map, {}))
     )
 )
 
---
+++ src/components/Map.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { translate } from '../lib/I18n'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+// import classNames from 'classnames'
+import '../styles/app'
+import 'leaflet/dist/leaflet.css'
+
+class MapTraces extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 13
+    }
+  }
+
+  render () {
+    const position = [this.state.lat, this.state.lng]
+    return (
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>A pretty CSS3 popup. <br /> Easily customizable.</span>
+          </Popup>
+        </Marker>
+      </Map>
+    )
+  }
+}
+
+export default translate()(MapTraces)
