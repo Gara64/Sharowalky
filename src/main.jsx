2c8ecd099235c258e3380b64f2844a949c850585
Gara64 <paul@cozycloud.cc>
2017-05-31 13:55:17 +0200

    Add cozy bar and client

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './styles/main'
 import React from 'react'
 import { render } from 'react-dom'
 
+
 const renderApp = function () {
   const Root = require('./components/Root').default
   render(<Root />, document.querySelector('[role=application]'))
@@ -14,4 +15,21 @@ if (module.hot) {
   module.hot.accept('./components/Root', () => requestAnimationFrame(renderApp))
 }
 
-document.addEventListener('DOMContentLoaded', renderApp)
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.querySelector('[role=application]')
+  const data = root.dataset
+
+  cozy.client.init({
+    cozyURL: '//' + data.cozyDomain,
+    token: data.cozyToken
+  })
+
+  cozy.bar.init({
+    appName: data.cozyAppName,
+    iconPath: data.cozyIconPath,
+    lang: data.cozyLocale,
+    replaceTitleOnMobile: true
+  })
+
+  renderApp()
+})
