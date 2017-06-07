bc6cbbb4526672a28480fb473341a00efb37a209
Gara64 <paul@cozycloud.cc>
2017-06-07 17:31:31 +0200

    Add planning

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -12,19 +12,72 @@ class Calendar extends React.Component {
     super(props)
   }
 
-  componentWillMount() {
-    const date = moment('2011-10-21')
-    this.setState({
-      date: date
-    })
-  }
-    render() {
-      return (<Dayz
-       display='day'
-       date={this.state.date}
-       />
-     )
-  }
+  render() {
+    console.log('date : ' + this.props.date)
+    const title = '<h3> My planning for this day ' + this.props.date + ' </h3>'
+    return (
+      <div>
+        <h3> My planning for this day</h3>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Hour</th>
+              <th>Tracks</th>
+              <th>Place</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="field-label col-md-3">
+                09:00 - 10:30
+              </td>
+              <td class="col-md-3">
+                Keynote
+              </td>
+              <td class="col-md-3">
+                Room 1
+              </td>
+            </tr>
+            <tr>
+              <td class="field-label col-md-3">
+                11:00 - 12:30
+              </td>
+              <td class="col-md-3">
+                Security & Privacy
+              </td>
+              <td class="col-md-3">
+                Room 3
+              </td>
+            </tr>
+            <tr>
+              <td class="field-label col-md-3">
+                14:00 - 15:30
+              </td>
+              <td class="col-md-3">
+                Persasive computing
+              </td>
+              <td class="col-md-3">
+                Room 2
+              </td>
+            </tr>
+            <tr>
+              <td class="field-label">
+                16:00 - 17:30
+              </td>
+              <td class="col-md-3">
+                Ubiquitous Mobile
+              </td>
+              <td class="col-md-3">
+                Room 2
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+
+   )
+}
 }
 
 export default translate()(Calendar)
