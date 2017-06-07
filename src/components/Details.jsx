110636b97ef0e47b9e23d3f05d0dd76efff0b245
Gara64 <paul@cozycloud.cc>
2017-06-07 17:31:19 +0200

    Add details

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -7,9 +7,28 @@ import mermaid from 'mermaid'
 import Highlight from 'react-highlight'
 
 class Details extends React.Component {
+  constructor (props) {
+    super(props)
+    mermaid.initialize({
+      startOnLoad: true
+    })
+    this.state = {
+      showFullTree: false
+    }
+    this.handleShowTreeChange = this.handleShowTreeChange.bind(this)
+  }
+
+  handleShowTreeChange () {
+    console.log('tree state changed')
+  }
 
   componentDidMount () {
     this.render()
+  }
+
+  handleShowTreeChange (event) {
+    let div = document.getElementById('tree')
+    div.setAttribute("style", "visibility: visible;")
   }
 
   render () {
@@ -29,6 +48,101 @@ class Details extends React.Component {
               <h4>Operators </h4>
               <div className='mermaid'>
                 graph BT;
+                  D(What: Photos)-->ACL;
+                  S(Who: Contacts)-->ACL;
+                  click ACL ACLCallback "Show ACL";
+              </div>
+              <button type="button" class="btn btn-info" onClick={this.handleShowTreeChange}>Show details</button>
+
+            </div>
+            <div className='col-md-9'>
+              <h4>Details</h4>
+              <div id='details'></div>
+              <div id='tree' style='visibility: hidden;'>
+                <OpTree />
+              </div>
+            </div>
+          </div>
+          </div>
+    )
+  }
+}
+
+class Code extends React.Component {
+  render () {
+    console.log('code : ', this.props.code)
+    return (
+      <div>
+        <p>{this.props.title}</p>
+        <Highlight className='json'>
+          {this.props.code}
+        </Highlight>
+      </div>
+    )
+  }
+}
+
+class OpTree extends React.Component {
+  render () {
+    return (
+      <div className='mermaid'>
+        graph BT;
+          PC1(Database)-->F1;
+          PC2(Database)-->F2;
+          F1(Filter1)-->DI1;
+          F2(Filter2)-->DI2;
+          DI1(DI)-->IS;
+          IS(IsS)-->SI;
+          DI2(DI)-->MS;
+          SI-->MS;
+          MS(MatchS)-->ACL;
+          click PC1 graphCallback "Personal Cloud Database";
+          click PC2 graphCallback "Personal Cloud Database";
+          click F1 graphCallback "Rule Filter";
+          click F2 graphCallback "Rule Filter";
+          click DI1 graphCallback "Document Identifiee";
+          click DI2 graphCallback "Document Identifiee";
+          click IS graphCallback "Subject selection";
+          click SI graphCallback "Subject extraction";
+          click MS graphCallback "Subject matching";
+          style IS fill:#ccf,stroke:#f66,stroke-width:2px,stroke-dasharray: 5, 5;
+          style SI fill:#ccf,stroke:#f66,stroke-width:2px,stroke-dasharray: 5, 5;
+          style MS fill:#ccf,stroke:#f66,stroke-width:2px,stroke-dasharray: 5, 5;
+
+      </div>
+    )
+  }
+}
+
+class ACL extends React.Component {
+  render () {
+
+    return (
+      <div>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Docs</th>
+              <th>Subjects</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <a href='http://cozy.tools:8080/files/downloads/adeb5c82ef3fccbf/petrus_phds.jpg'>Petrus_phds.jpg</a>
+              </td>
+              <td>
+                Riad
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+                /*graph BT;
                   PC1(Database)-->F1;
                   PC2(Database)-->F2;
                   F1(Filter1)-->DI1;
@@ -47,34 +161,18 @@ class Details extends React.Component {
                   click IS graphCallback "Subject selection";
                   click SI graphCallback "Subject extraction";
                   click MS graphCallback "Subject matching";
-              </div>
-            </div>
-            <div className='col-md-9'>
-              <h4>Details</h4>
-              <div id='details'></div>
-            </div>
-          </div>
-          </div>
-    )
-  }
-}
-
-class Code extends React.Component {
-  render () {
-    console.log('code : ', this.props.code)
-
-    return (
-      <div>
-        <p>{this.props.title}</p>
-        <Highlight className='json'>
-          {this.props.code}
-        </Highlight>
-      </div>
-    )
-  }
-}
+                  */
 
 export default translate()(Details)
+
+window.ACLCallback = function(id) {
+  console.log('id: ' + id)
+  let div = document.getElementById('details')
+  div.innerHTML = '' // reset to avoid buggy HighLight render
+  ReactDOM.render(<ACL />, div)
+
+
+}
 
 window.graphCallback = function (id) {
   let div = document.getElementById('details')
